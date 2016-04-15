f553201375d1980058b6fab15047f94b547ce46e
Brandonsmith23 <Brandonsmith@newmediadenver.com>
2016-04-15 10:34:44 -0600

    sticky sidebars

--- docroot/content/themes/g6pd/assets/js/_main.js
+++ docroot/content/themes/g6pd/assets/js/_main.js
@@ -6,7 +6,7 @@
  * replace the dash with an underscore when adding it to the object below.
  *
  * .noConflict()
- * The routing is enclosed within an anonymous function so that you can 
+ * The routing is enclosed within an anonymous function so that you can
  * always reference jQuery with $, even when in .noConflict() mode.
  *
  * Google CDN, Latest jQuery
@@ -16,13 +16,39 @@
 
 (function($) {
 
-// Use this variable to set up the common and page specific functions. If you 
+// Use this variable to set up the common and page specific functions. If you
 // rename this variable, you will also need to rename the namespace below.
 var Roots = {
   // All pages
   common: {
     init: function() {
-      // JavaScript to be fired on all pages
+        $(function(){ // document ready
+
+        var stickyTop = $('#what-is').offset().top; // returns number
+        var windowTop = $(window).scrollTop(); // returns number
+        var headerHeight = $('.banner').height();
+        var stickyPoint = stickyTop - headerHeight - 32;
+
+        $(window).scroll(function(){ // scroll event
+
+          // console.log($(window).scrollTop());
+          // console.log(stickyPoint);
+          // console.log(headerHeight);
+
+          if ( stickyPoint < $(window).scrollTop() ) {
+            $('.sub-menu').css({ position: 'fixed', top: 160, left: 68, width: 285 });
+            $('.side-content').css({ position: 'fixed', top: 160, right: 18, paddingRight: 35, marginRight: 16 });
+            $('.margin-fixed').css({ marginLeft: '285px' });
+          }
+          else {
+            $('.sub-menu').css({ position: 'static', top: 0, left: 0, width: '25%' });
+            $('.side-content').css({ position: 'static', top: 0, right: 0, paddingRight: 0, marginRight: 0 });
+            $('.margin-fixed').css({ marginLeft: 0, paddingLeft: 0 });
+          }
+
+        });
+
+      });
     }
   },
   // Home page
