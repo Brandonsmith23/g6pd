79e8743e6bff38d3294788414aa4e72ce46bb7de
Devin Kendall <dkendall@Devins-MacBook-Pro.local>
2016-07-11 05:18:17 -0600

    audit stylings

--- docroot/content/themes/g6pd/templates/header.php
+++ docroot/content/themes/g6pd/templates/header.php
@@ -1,18 +1,19 @@
 <header class="banner navbar navbar-default navbar-static-top" role="banner">
   <div class="container">
     <div class="navbar-header row">
-      <a class="navbar-brand" href="<?php echo esc_url(home_url('/')); ?>"><img src=<?php echo get_stylesheet_directory_uri().'/assets/img/G6PD-Logo.png'; ?>></a>
-      <a class="donate-button" href="<?php echo get_permalink( get_page_by_title( 'Donate Today' ) ) ?>">Make a Donation</a>
-    </div>
-
-    <div class="row">
-      <nav role="navigation">
-        <?php
-          if (has_nav_menu('primary_navigation')) :
-            wp_nav_menu(array('theme_location' => 'primary_navigation', 'walker' => new Roots_Nav_Walker(), 'menu_class' => 'nav navbar-nav'));
-          endif;
-        ?>
-      </nav>
+      <div class="col-md-3">
+        <a class="navbar-brand" href="<?php echo esc_url(home_url('/')); ?>"><img src=<?php echo get_stylesheet_directory_uri().'/assets/img/G6PD-Logo.png'; ?>></a>
+      </div>
+      <div class="col-md-9">
+        <nav role="navigation">
+          <!-- <a class="donate-button" href="<?php echo get_permalink( get_page_by_title( 'Donate Today' ) ) ?>">Make a Donation</a> -->
+            <?php
+              if (has_nav_menu('primary_navigation')) :
+                wp_nav_menu(array('theme_location' => 'primary_navigation', 'walker' => new Roots_Nav_Walker(), 'menu_class' => 'nav navbar-nav'));
+              endif;
+            ?>
+        </nav>
+      </div>
 
     </div>
 
@@ -20,4 +21,4 @@
 </header>
 
 <?php
-  load_template(STYLESHEETPATH.'/templates/components/masthead.php');
+  // load_template(STYLESHEETPATH.'/templates/components/masthead.php');
